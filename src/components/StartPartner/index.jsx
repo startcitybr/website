119e37d76fc124c58ab5736711930bffa02009ee
fnjorge <fred@insiders.com.br>
2024-04-20 00:07:58 -0300

    Google Forms

--- src/components/StartPartner/index.jsx
+++ src/components/StartPartner/index.jsx
@@ -16,17 +16,17 @@ export const StartPartner = () => {
         <div className="bg-darkcolor flex-1 p-16 w-96 lg:rounded-l-xl">
             <h3 className="text-white py-4 text-xl font-medium uppercase">Cadastrar projeto</h3>
             <p className="text-graycolor mb-8">Clique no link abaixo para cadastrar seu projeto na Starcity</p>
-            <a className="uppercase text-sm text-white">Cadastrar-se agora</a>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLScB23oWC1rZzAvyTFhJ-cXHdb1u4dMkr8nVX8dL3jxsh2MjvA/viewform" target="_blank" className="uppercase text-sm text-white">Cadastrar-se agora</a>
         </div>
         <div className="bg-maincolor2 flex-1 p-16 w-96">
         <h3 className="text-white py-4 text-xl font-medium uppercase">Seja um investidor</h3>
         <p className="text-graycolor mb-8">Clique no link abaixo para cadastrar-se como um investidor</p>
-        <a className="uppercase text-sm text-white">Cadastrar-se agora</a>
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfOalbdloFGBfX4hM-SU5NQA8YWMTthCDS5DjrXsHmwdJx_EQ/viewform" target="_blank" className="uppercase text-sm text-white">Cadastrar-se agora</a>
         </div>
         <div className="bg-maincolor flex-1 p-16 w-96 lg:rounded-r-xl">
         <h3 className="text-white py-4 text-xl font-medium uppercase">Seja um parceiro</h3>
         <p className="text-graycolor mb-8">Clique no link abaixo para cadastrar-se como parceiro</p>
-        <a className="uppercase text-sm text-white">Cadastrar-se agora</a>
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSdrF3s8IShSjWOr2nPtij5266h-PznsQ2OUrUcgeu_NAgAiZw/viewform" target="_blank" className="uppercase text-sm text-white">Cadastrar-se agora</a>
         </div>
       </div>
     </div>
